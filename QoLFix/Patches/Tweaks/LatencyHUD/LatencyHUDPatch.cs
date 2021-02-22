84708b3c1148489506a0f7d765bfe22617c04946
peelz <peelz.dev@gmail.com>
2021-02-21 19:59:10 -0500

    LatencyHUD: add ping popup when hovering on player names

--- QoLFix/Patches/Tweaks/LatencyHUD/LatencyHUDPatch.cs
+++ QoLFix/Patches/Tweaks/LatencyHUD/LatencyHUDPatch.cs
@@ -1,6 +1,11 @@
-﻿using BepInEx.Configuration;
+﻿using System;
+using System.Linq;
+using BepInEx.Configuration;
+using CellMenu;
 using HarmonyLib;
+using QoLFix.Patches.Common;
 using SNetwork;
+using TMPro;
 using UnhollowerRuntimeLib;
 using UnityEngine;
 
@@ -32,6 +37,8 @@ namespace QoLFix.Patches.Tweaks
         private static readonly string PatchName = nameof(LatencyHUDPatch);
         private static readonly ConfigDefinition ConfigEnabled = new ConfigDefinition(PatchName, "Enabled");
 
+        private static readonly Vector3 PopupCursorOffset = new Vector3(0, 5f, 0);
+
         public static IPatch Instance { get; private set; }
 
         public void Initialize()
@@ -48,9 +55,122 @@ namespace QoLFix.Patches.Tweaks
 
         public void Patch()
         {
-            ClassInjector.RegisterTypeInIl2Cpp<LatencyHUDElement>();
+            ClassInjector.RegisterTypeInIl2Cpp<LatencyWatermark>();
             this.PatchMethod<WatermarkGuiLayer>(nameof(WatermarkGuiLayer.Setup), new[] { typeof(Transform), typeof(string) }, PatchType.Postfix);
             //this.PatchMethod<SNet_Core_STEAM>(nameof(SNet_Core_STEAM.UpdateConnectionStatus), PatchType.Postfix);
+
+            QoLFixPlugin.RegisterPatch<PlayerNameExtPatch>();
+            PlayerNameExtPatch.CursorUpdate += OnCursorUpdate;
+        }
+
+        private GameObject popupGO;
+        private TextMeshPro pingText;
+        private SpriteRenderer bgSprite;
+
+        private void InitializePopup(CM_PageBase page)
+        {
+            popupGO = GOFactory.CreateObject("PlayerInfoPopup", null, out RectTransform t);
+
+            popupGO.layer = LayerManager.LAYER_UI;
+            popupGO.SetActive(false);
+
+            t.anchorMin = new Vector2(0.5f, 0.5f);
+            t.anchorMax = new Vector2(0.5f, 0.5f);
+            t.pivot = new Vector2(0, 1);
+            t.offsetMin = Vector2.zero;
+            t.offsetMax = Vector2.zero;
+            t.localPosition = Vector2.zero;
+
+            var bgGO = GOFactory.CreateObject("Background", popupGO.transform,
+                out RectTransform bgTransform,
+                out bgSprite);
+            bgGO.layer = LayerManager.LAYER_UI;
+
+            bgSprite.sortingOrder = 50;
+            bgSprite.color = new Color(0.4f, 0.4f, 0.4f, 1);
+
+            bgTransform.pivot = new Vector2(0, 1);
+            bgTransform.localScale = new Vector3(31f, 8f, 1f);
+            bgTransform.localPosition = Vector2.zero;
+
+            var tex = Resources.Load<Texture2D>("gui/gear/frames/cellUI_Frame_BoxFiled");
+            bgSprite.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), t.pivot, 100f);
+
+            var textGO = GOFactory.CreateObject("Text", popupGO.transform,
+                out RectTransform textTransform,
+                out pingText,
+                out Cell_TMProDisabler _,
+                out LatencyWatermark _);
+            textGO.layer = LayerManager.LAYER_UI;
+
+            var techFont = page.GetComponentsInChildren<TextMeshPro>()
+                .FirstOrDefault(x => x.font?.name.Contains("ShareTechMono") == true)
+                ?.font;
+
+            if (techFont == null)
+            {
+                this.LogError("Failed to find font 'ShareTechMono'");
+            }
+
+            pingText.color = Color.white;
+            pingText.alpha = 1f;
+            pingText.fontSize = 18;
+            pingText.font = techFont;
+            pingText.alignment = TextAlignmentOptions.TopLeft;
+            pingText.enableWordWrapping = false;
+            pingText.isOrthographic = true;
+            pingText.autoSizeTextContainer = true;
+            pingText.sortingOrder = bgSprite.sortingOrder + 1;
+            pingText.UpdateMaterial();
+
+            textTransform.offsetMin = Vector2.zero;
+            textTransform.offsetMax = Vector2.zero;
+            textTransform.pivot = new Vector2(0, 1);
+            textTransform.localPosition = Vector2.zero;
+        }
+
+        private float pingTextWidth;
+        private float pingTextHeight;
+        private bool lastState;
+
+        private void OnCursorUpdate(CM_PageBase page, Vector2 cursorPos, ref RaycastHit2D rayHit, bool hovering, bool clicked, Lazy<SNet_Player> player)
+        {
+            if (this.popupGO == null)
+            {
+                InitializePopup(page);
+            }
+
+            if (hovering) UpdatePosition();
+
+            if (this.lastState == hovering) return;
+            lastState = hovering;
+
+            if (!hovering)
+            {
+                this.popupGO.SetActive(false);
+                return;
+            }
+
+            this.popupGO.transform.SetParent(page.transform, false);
+            this.popupGO.SetActive(true);
+
+            this.pingText.SetText(GetPlayerPing(player.Value));
+            this.pingText.ForceMeshUpdate(true);
+            this.pingTextWidth = pingText.GetRenderedWidth(false);
+            this.pingTextHeight = pingText.GetRenderedHeight(false);
+
+            // There's invisible pixels on the background sprite, so we need
+            // to account for that when centering
+            const float bgOffset = 8f;
+            var gap = this.bgSprite.bounds.size.x - this.pingTextWidth;
+            this.pingText.transform.localPosition = new Vector3(gap / 2f + bgOffset, 0, 0);
+
+            UpdatePosition();
+
+            void UpdatePosition()
+            {
+                popupGO.transform.position = PopupCursorOffset + new Vector3(cursorPos.x, cursorPos.y + this.pingTextHeight / 2f, this.popupGO.transform.position.z);
+            }
         }
 
         private static void SNet_Core_STEAM__UpdateConnectionStatus__Postfix(SNet_Player player)
@@ -58,7 +178,69 @@ namespace QoLFix.Patches.Tweaks
             Instance.LogDebug("UpdateConnectionStatus: " + player?.NickName + " " + player?.Ping);
         }
 
-        private static void WatermarkGuiLayer__Setup__Postfix(WatermarkGuiLayer __instance) =>
-            LatencyHUDElement.Patch(__instance.m_watermark);
+        private static void WatermarkGuiLayer__Setup__Postfix(WatermarkGuiLayer __instance)
+        {
+            var watermark = __instance.m_watermark;
+
+            var go = GOFactory.CreateObject("Latency", watermark.transform,
+                    out RectTransform t,
+                    out TextMeshPro text,
+                    out Cell_TMProDisabler _,
+                    out LatencyWatermark _);
+            go.layer = LayerManager.LAYER_UI;
+
+            var fpsText = watermark.m_fpsText;
+            var statusTransform = watermark.m_statusText.GetComponent<RectTransform>();
+
+            t.position = statusTransform.position;
+            t.localPosition = statusTransform.localPosition;
+            t.localScale = statusTransform.localScale;
+            t.anchorMin = statusTransform.anchorMin;
+            t.anchorMax = statusTransform.anchorMax;
+            t.pivot = statusTransform.pivot;
+            t.offsetMax = statusTransform.offsetMax;
+            t.offsetMin = statusTransform.offsetMin;
+            t.anchoredPosition = statusTransform.anchoredPosition;
+
+            text.isOrthographic = fpsText.isOrthographic;
+            text.font = fpsText.font;
+            text.color = fpsText.color;
+            text.alpha = fpsText.alpha;
+            text.fontSize = fpsText.fontSize;
+            text.fontSizeMin = fpsText.fontSizeMin;
+            text.fontSizeMax = fpsText.fontSizeMax;
+            text.fontWeight = fpsText.fontWeight;
+            text.fontStyle = fpsText.fontStyle;
+            text.enableKerning = fpsText.enableKerning;
+            text.enableWordWrapping = false;
+            text.alignment = fpsText.alignment;
+            text.autoSizeTextContainer = fpsText.autoSizeTextContainer;
+            text.UpdateMaterial();
+            text.UpdateFontAsset();
+        }
+
+        private static string GetPlayerPing(SNet_Player player)
+        {
+            // This seems to return the same thing as SNet_Player.Ping
+            //SNet.MasterManagement.GetPing(player, ref ping, ref quality);
+
+            int? latency = null;
+            if (player != null && !player.IsLocal)
+            {
+                latency = player?.Ping;
+            }
+            else if (SNet.Master != null)
+            {
+                latency = 0;
+                if (!SNet.Master.IsLocal)
+                {
+                    latency = SNet.Master.Ping;
+                    //var pingLocation = SNet.Master.Load<pPingLocation>().pingLocation;
+                    //latency = SteamNetworkingUtils.EstimatePingTimeFromLocalHost(ref pingLocation);
+                }
+            }
+
+            return $"{latency?.ToString() ?? "???"} ms";
+        }
     }
 }
---
+++ QoLFix/Patches/Tweaks/LatencyHUD/LatencyWatermark.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Player;
+using SNetwork;
+using TMPro;
+using UnityEngine;
+
+namespace QoLFix.Patches.Tweaks
+{
+    public partial class LatencyHUDPatch
+    {
+        public class LatencyWatermark : MonoBehaviour
+        {
+            public LatencyWatermark(IntPtr value)
+                : base(value) { }
+
+            private float lastUpdated;
+            private PUI_Watermark watermark;
+            private TextMeshPro text;
+
+            public PlayerAgent Player { get; set; }
+
+            private void Awake()
+            {
+                this.text = this.GetComponent<TextMeshPro>();
+            }
+
+            private void Update()
+            {
+                if ((Time.time - this.lastUpdated) < 0.25f) return;
+                this.lastUpdated = Time.time;
+
+                if (this.watermark == null)
+                {
+                    this.watermark = this.GetComponentInParent<PUI_Watermark>();
+                }
+                if (this.watermark == null) return;
+
+                // Hide when we're not connected
+                if (!SNet.HasMaster)
+                {
+                    this.text.enabled = false;
+                    return;
+                }
+
+                var statusShown = this.watermark.m_statusText?.isActiveAndEnabled == true;
+                var fpsShown = this.watermark.m_fpsText?.isActiveAndEnabled == true;
+
+                // Hide when StatusText is shown
+                if (statusShown)
+                {
+                    this.text.enabled = false;
+                    return;
+                }
+                else
+                {
+                    this.text.enabled = true;
+                }
+
+                this.text.transform.position = fpsShown
+                    ? this.watermark.m_statusText.transform.position
+                    : this.watermark.m_fpsText.transform.position;
+
+                this.text.SetText(GetPlayerPing(this.Player?.Owner));
+                this.text.ForceMeshUpdate(true);
+            }
+        }
+    }
+}
