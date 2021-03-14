43093b7574f8b9a0074100f1b47d8569f5c53894
peelz <peelz.dev@gmail.com>
2021-03-14 10:10:49 -0400

    Add workaround for property init for netstandard2.1

---
+++ QoLFix/System.Runtime.CompilerServices.IsExternalInit.cs
@@ -0,0 +1,17 @@
+﻿#if NETSTANDARD2_1
+// This is necessary because C# 9.0 init properties require a class that isn't
+// included in netstandard <= 2.1
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Reserved to be used by the compiler for tracking metadata.
+    /// This class should not be used by developers in source code.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal static class IsExternalInit
+    {
+    }
+}
+#endif
