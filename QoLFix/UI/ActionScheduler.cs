ef2cb1ee9d4d189fb3e05b2c5efe1854849b5046
peelz <peelz.dev@gmail.com>
2021-04-21 20:28:56 -0400

    Add ActionScheduler.Repeat

--- QoLFix/UI/ActionScheduler.cs
+++ QoLFix/UI/ActionScheduler.cs
@@ -8,28 +8,37 @@ namespace QoLFix.UI
     {
         private class ActionEntry : IScheduledAction
         {
-            public ActionEntry(Action action, float delay)
+            public ActionEntry(Action action, float delay, float interval)
             {
                 this.action = action;
                 this.delay = delay;
+                this.interval = interval;
             }
 
             public Action action;
             public float delay;
+            public float interval;
             public bool enabled;
             public bool completed;
             public bool markedForDeletion;
 
             public void Invalidate() => this.markedForDeletion = true;
 
-            public bool Active => !this.completed && !this.markedForDeletion;
+            public bool Active => (!this.completed || this.interval >= 0) && !this.markedForDeletion;
         }
 
         private static readonly List<ActionEntry> Actions = new(64);
 
         public static IScheduledAction Schedule(Action action, float delay = 0)
         {
-            var entry = new ActionEntry(action, delay);
+            var entry = new ActionEntry(action, delay, -1);
+            Actions.Add(entry);
+            return entry;
+        }
+
+        public static IScheduledAction Repeat(Action action, float interval = 0)
+        {
+            var entry = new ActionEntry(action, interval, interval);
             Actions.Add(entry);
             return entry;
         }
@@ -66,7 +75,8 @@ namespace QoLFix.UI
                     QoLFixPlugin.LogError($"An exception was caught while running a scheduled action: {ex}");
                 }
 
-                Actions.RemoveAt(i);
+                if (entry.interval < 0) Actions.RemoveAt(i);
+                else entry.delay = entry.interval;
             }
         }
     }
