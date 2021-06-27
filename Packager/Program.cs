49bd9d82a9288ff08287f1c746e147322d4c7c1e
peelz <peelz.dev@gmail.com>
2021-06-27 05:49:07 -0400

    Add packaging script for Thunderstore modpack

--- Packager/Program.cs
+++ Packager/Program.cs
@@ -1,11 +1,114 @@
 using System;
+using System.CommandLine;
+using System.CommandLine.IO;
+using System.CommandLine.Parsing;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using MTFO.ThunderstorePackager;
 
 namespace Packager
 {
     internal static class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            var outputOption = new Option<DirectoryInfo>(
+                "--output",
+                arity: ArgumentArity.ExactlyOne,
+                description: "Output folder where the packages will get saved to.").LegalFilePathsOnly();
+            var readmeOption = new Option<FileInfo>(
+                "--readme",
+                arity: ArgumentArity.ExactlyOne,
+                description: "Readme file to include in modpack package.").ExistingOnly();
+            var iconOption = new Option<FileInfo>(
+                "--icon",
+                arity: ArgumentArity.ExactlyOne,
+                description: "Icon file to include in the modpack package.").ExistingOnly();
+            var nameOption = new Option<string>(
+                "--name",
+                arity: ArgumentArity.ExactlyOne,
+                description: "Name of the modpack package.");
+            var authorOption = new Option<string>(
+                "--author",
+                arity: ArgumentArity.ExactlyOne,
+                description: "Author of the modpack package.");
+            var descriptionOption = new Option<string>(
+                "--description",
+                arity: ArgumentArity.ZeroOrOne,
+                description: "Description of the modpack package.");
+            var versionOption = new Option<string>(
+                "--version",
+                arity: ArgumentArity.ExactlyOne,
+                description: "Version of the modpack package.");
+            var websiteOption = new Option<string>(
+                "--website",
+                arity: ArgumentArity.ZeroOrOne,
+                description: "Website url of the modpack package.");
+            var dependencyOption = new Option<string>(
+                "--dependency",
+                arity: ArgumentArity.ZeroOrMore,
+                description: "Dependencies of the modpack package.");
+            var rootCommand = new RootCommand
+            {
+                outputOption,
+                readmeOption,
+                iconOption,
+                nameOption,
+                authorOption,
+                descriptionOption,
+                versionOption,
+                websiteOption,
+                dependencyOption,
+            };
+
+            var console = new SystemConsole();
+            var parser = new Parser(rootCommand);
+            var result = parser.Parse(args);
+
+            var output = result.FindResultFor(outputOption)?.GetValueOrDefault<DirectoryInfo>();
+            var readme = result.FindResultFor(readmeOption)?.GetValueOrDefault<FileInfo>();
+            var icon = result.FindResultFor(iconOption)?.GetValueOrDefault<FileInfo>();
+            var name = result.FindResultFor(nameOption)?.GetValueOrDefault<string>();
+            var author = result.FindResultFor(authorOption)?.GetValueOrDefault<string>();
+            var description = result.FindResultFor(descriptionOption)?.GetValueOrDefault<string>();
+            var version = result.FindResultFor(versionOption)?.GetValueOrDefault<string>();
+            var website = result.FindResultFor(websiteOption)?.GetValueOrDefault<string>();
+            var dependencies = result.FindResultFor(dependencyOption)?.GetValueOrDefault<string[]>();
+
+            if (!Required(nameof(output), output)
+                || !Required(nameof(readme), readme)
+                || !Required(nameof(icon), icon)
+                || !Required(nameof(name), name)
+                || !Required(nameof(author), author)
+                || !Required(nameof(version), version)) return 1;
+
+            Directory.CreateDirectory(output.FullName);
+
+            var pkg = new ThunderstorePackage(Path.Combine(output.FullName, "modpack.zip"), new()
+            {
+                Icon = new FileReference(icon.FullName),
+                Readme = new FileReference(readme.FullName),
+                Manifest = new()
+                {
+                    Name = name,
+                    Namespace = author,
+                    Description = description ?? "",
+                    Version = version,
+                    WebsiteUrl = website ?? "",
+                    Dependencies = dependencies ?? Array.Empty<string>(),
+                },
+            });
+
+            return 0;
+
+            bool Required<T>(string name, [NotNull] T? value) where T : class
+            {
+                if (typeof(T) == typeof(string)
+                    ? (string?)(object?)value != ""
+                    : value != null) return true;
+                console.Error.Write($"Mandatory option '--{name}' is not specified.");
+                return false;
+            }
         }
     }
 }
--- build.sh
+++ build.sh
@@ -1,8 +1,101 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 SCRIPT="$(readlink -f "$BASH_SOURCE")"
 BASEDIR="$(dirname "$SCRIPT")"
 
-dotnet run --project "$BASEDIR/Packager" --configuration Debug
-dotnet run --project "$BASEDIR/Packager" --configuration Release-Standalone
-dotnet run --project "$BASEDIR/Packager" --configuration Release-Thunderstore
+case "$(uname -s)" in
+  CYGWIN*) isCygwin=1;;
+  MINGW*)  isCygwin=1;;
+  *)       isCygwin=0;;
+esac
+
+function join_by { local d=${1-} f=${2-}; if shift 2; then printf %s "$f" "${@/#/$d}"; fi; }
+
+function checkBin() {
+  local bin="$1"
+  local binpath exitcode
+
+  binpath="$(command -v "$bin")"
+  exitcode="$?"
+
+  if [[ "$exitcode" -ne "0" ]]; then
+    echo "Unable to find binary '$bin' in your PATH"
+    return 1
+  fi
+
+  [[ -x "$binpath" ]] && return 0
+
+  echo "Binary '$bin' located at '$binpath' is not executable"
+  return 1
+}
+
+checkBin dotnet || exit 1
+checkBin unzip || exit 1
+checkBin jq || exit 1
+
+tmp_build_dir="$(mktemp -d)"
+if [[ "$isCygwin" -eq 1 ]]; then
+  tmp_build_dir="$(cygpath -w "$tmp_build_dir")"
+fi
+
+trap "{ exitcode=\$?; rm -rf \"$tmp_build_dir\"; exit \$exitcode; }" EXIT
+
+packager_dll="$tmp_build_dir/packager/Packager.dll"
+modpack_readme="$tmp_build_dir/packager/README.md"
+
+MODPACK_NAME="QoLFix"
+MODPACK_AUTHOR="notpeelz"
+MODPACK_VERSION="1.0.0"
+MODPACK_DESCRIPTION="A general GTFO improvement mod that aims to fix various quality of life issues."
+MODPACK_WEBSITE="https://github.com/notpeelz/GTFO-QoLFix"
+MODPACK_DEPENDENCY_BLACKLIST=()
+README_PROJECT_REFERENCE=("$BASEDIR/QoL."*)
+
+# Only keep the Qol.* folder names
+for i in "${!README_PROJECT_REFERENCE[@]}"; do
+  README_PROJECT_REFERENCE[$i]="$(basename ${README_PROJECT_REFERENCE[$i]})"
+done
+
+rm -rf "$BASEDIR/build"
+
+# Build mod packages
+dotnet build -c Release-Thunderstore -o "$tmp_build_dir/pkgs"
+mkdir -p "$BASEDIR/build/pkgs"
+cp "$tmp_build_dir/pkgs/thunderstore/"* "$BASEDIR/build/pkgs"
+
+# Aggregate dependency strings from from manifests
+MODPACK_DEPENDENCY=()
+for zip in "$BASEDIR/build/pkgs/"*.zip; do
+  [[ -z "$zip" ]] && continue
+
+  name="$(unzip -p "$zip" manifest.json | jq -r '.name')"
+
+  # Skip if the mod is blacklisted from the dependency list
+  if [[ " ${MODPACK_DEPENDENCY_BLACKLIST[@]} " =~ " $name " ]]; then
+    echo "Skipping dependency: $name"
+    continue
+  fi
+
+  echo "Adding dependency: $name"
+
+  depStr="$(unzip -p "$zip" manifest.json | jq -r '"\(.namespace)-\(.name)-\(.version_number)"')"
+  MODPACK_DEPENDENCY+=("$depStr")
+done
+
+# Build modpack package
+dotnet msbuild -restore "$BASEDIR/Packager" \
+  -p:"Configuration=Release-Thunderstore" \
+  -p:"OutputPath=\"$tmp_build_dir/packager\"" \
+  -p:"RanFromBuildScript=true" \
+  -p:"ReadmeProjectReferences=\"$(join_by ';' "${README_PROJECT_REFERENCE[@]}")\""
+dotnet "$packager_dll" \
+  --output "$BASEDIR/build" \
+  --icon "$BASEDIR/img/logo.png" \
+  --readme "$modpack_readme" \
+  --name "$MODPACK_NAME" \
+  --author "$MODPACK_AUTHOR" \
+  --description "$MODPACK_DESCRIPTION" \
+  --website "$MODPACK_WEBSITE" \
+  --version "$MODPACK_VERSION" \
+  --dependency "${MODPACK_DEPENDENCY[@]}"
